3a5a4e3eac0a169d9fa584fed3ad9b1c04d59daa
AnthonyLaurent1 <anthony061214@gmail.com>
2022-09-25 14:48:34 +0200

    test paginator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import File from '../../assets/resources/__credits.json';
 
 @Component({
@@ -7,14 +7,37 @@ import File from '../../assets/resources/__credits.json';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @Input() fileName = File;
+  fileName = File;
 
   key = Object.keys(this.fileName);
 
-  constructor() { }
+  itemPerPage : number[] = [10, 20, 50]
+
+  page: any = localStorage.getItem('page');
+
+  array50: string[][] = this.sortArray(this.key, 50, 32, 50, 50);
+  array20: string[][]    = this.sortArray(this.key, 20, 2, 20, 20);
+  array10: string[][]    = this.sortArray(this.key, 10, 2, 10, 10)
+
+  setNumberItemPerPage(n: string) {
+    localStorage.setItem('page', n)
+    location.reload()
+  }
+
+  sortArray(key: string[], start: number, length: number, incre: number, slice: number): string[][] {
+    let array: string[][] = []
+
+    for (let i = start; i < key.length + length ; i+=incre) {
+      let e = key.slice(i-slice, i)
+      array.push(e)
+    }
+    return array
+  }
+
+  constructor() {}
 
   ngOnInit(): void {
-    console.log(this.key)
+
   }
 
 }
